0b0f24b6e8161ca27611185eb6c0a3061b0bdbd3
rashmisona <rashmisonawanepict@gmail.com>
2016-07-09 14:45:11 +0530

    Adding new addition file

---
+++ addition.c
@@ -0,0 +1,12 @@
+int main(void) {
+int a,b;
+int c;
+printf("\nEnter the number1 : ");
+scanf("%d",&a);
+
+printf("\nEnter the number2 : ");
+scanf("%d",&b);
+c=a+b;
+printf("\n Addition : %d",c);
+return 0;
+}
