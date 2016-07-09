2f189909f7037d836cb701e8fc0e73c1a36784aa
rashmisona <rashmisonawanepict@gmail.com>
2016-07-09 14:52:35 +0530

    Adding div file

---
+++ Divion.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+int main(void) {
+int a,b;
+int c;
+printf("\nEnter the number1 : ");
+scanf("%d",&a);
+
+printf("\nEnter the number2 : ");
+scanf("%d",&b);
+c=a/b;
+printf("\n Divion: %d",c);
+return 0;
+}
+
